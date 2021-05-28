fcdcc83507a1d03b88063be0f4f8c46315fb427d
João Albernaz <joaohbalbernaz@gmail.com>
2021-05-27 21:34:43 -0300

    feat: add base for new project

---
+++ src/main/java/cirroseemgel/cirroseemgel/CirroseEmGelApplication.java
@@ -0,0 +1,13 @@
+package cirroseemgel.cirroseemgel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CirroseEmGelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CirroseEmGelApplication.class, args);
+	}
+
+}
