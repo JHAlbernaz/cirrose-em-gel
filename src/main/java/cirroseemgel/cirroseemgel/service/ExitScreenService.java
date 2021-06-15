67716f0ddb86a8649fb172a22da9aa6d9d4f058f
João Albernaz <joaohbalbernaz@gmail.com>
2021-06-15 09:17:32 -0300

    feat: change style of exit screen

--- src/main/java/cirroseemgel/cirroseemgel/service/ExitScreenService.java
+++ src/main/java/cirroseemgel/cirroseemgel/service/ExitScreenService.java
@@ -3,6 +3,13 @@ package cirroseemgel.cirroseemgel.service;
 public class ExitScreenService {
 
     public static void mainExitScreen() {
-        System.out.println("Volte sempre que quiser! :)");
+
+        System.out.println("+ --------------------------------- +");
+        System.out.println("|          Cirrose em Gel           |");
+        System.out.println("|                                   |");
+        System.out.println("|    Volte sempre que quiser! :)    |");
+        System.out.println("|                                   |");
+        System.out.println("+ --------------------------------- +");
+
     }
 }
